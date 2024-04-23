6a14592fa5d0aa1a34670168ca27c33b9eb6e185
EugeneSergeevich20 <eugene.chernitsyn15@gmail.com>
2024-04-23 16:07:44 +0400

    First-commit

---
+++ src/main/java/com/example/sserverapitest/SserverApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.sserverapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SserverApiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SserverApiTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sserverapitest/SserverApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sserverapitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SserverApiTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
